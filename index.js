ca3448e9077d00998eb56f5e648a7a9d5fe7598a
kelvynking <kelvyn.m.king@gmail.com>
2021-10-08 15:00:04 +1100

    Created the function deleteFromDriverByKey which deletes the key/value pair but does not mutate the original driver.

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
     driver[key] = value;
     return driver;
 }
+
+function deleteFromDriverByKey(driver, key) {
+    const newDriver = {...driver};
+    delete newDriver[key];
+    return newDriver;
+}
